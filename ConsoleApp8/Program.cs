127fba714b19d689b3355ac879b75519ca40fedc
Alforsan <Alforsan@DESKTOP-URCU90U>
2021-11-27 10:37:22 +0200

    Add project files.

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace AmirPrime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            Console.Write("Enter the Number to check Prime: ");
+            int A = int.Parse(Console.ReadLine());
+            int B = A / 2;
+            for (int E = 2; E <= B; E++)
+            {
+                if (A % E == 0)
+                {
+                    Console.Write("not Prime");
+                    count = 1;
+                    break;
+                }
+            }
+            if (count == 0)
+                Console.Write("Prime");
+
+            Console.ReadKey();
+        }
+    }
+}
+
